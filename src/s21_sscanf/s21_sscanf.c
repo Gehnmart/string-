172574e65923cf0260c9b6cb18d0d4226cee4952
Dina Humblebrag <dinahumb@in-b7.nsk.21-school.ru>
2023-11-27 23:05:00 +0700

    fkd

--- src/s21_sscanf/s21_sscanf.c
+++ src/s21_sscanf/s21_sscanf.c
@@ -96,9 +96,7 @@ int s21_sscanf(const char *str, const char *format, ...) {
   int id = 0, res = 0;
   va_list next_var;
   va_start(next_var, format);
-  param_t param;
-  tocen_is_er(&param);
-  param.n_specik = 0;
+  param_t param = {0};
   char mass[BUFF_MAX] = {'\0'};
   char *buff = (char *)mass;
   char *c = s21_NULL;
@@ -207,10 +205,6 @@ int s21_sscanf(const char *str, const char *format, ...) {
         case '%':
           str = parse_procent((char *)str, &param);
           break;
-      }
-      if (*c == s21_EOF) {
-        res = -1;
-        break;
       }
       if (c == s21_NULL) break;
       if (str == s21_NULL) break;
